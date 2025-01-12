bb5c2b50cde2695e834a2f225bfe80670f64889e
rani1405 <jdpatel029@gmail.com>
2025-01-12 17:46:54 -0500

    Kafka produce with 3 partitions and 3 replication factor, acks=all

---
+++ src/main/java/com/example/kafkaevent/KafkaEventAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEventAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEventAppApplication.class, args);
+	}
+
+}
